d8044f3073b54e61ddba26564ddef4dd6edf2ade
valcheb <cheb.valentin@gmail.com>
2021-01-26 10:08:06 +0300

    Add event name and callback typedefs

--- event_scheduler.h
+++ event_scheduler.h
@@ -1,6 +1,18 @@
 #ifndef EVENT_SCHEDULER_H_
 #define EVENT_SCHEDULER_H_
 
+#include <stdint.h>
 
+#define EVENT_NAME_LENGTH 10
+#define EVENT_ARRAY_SIZE  10
+
+typedef char event_name_t[EVENT_NAME_LENGTH];
+typedef void (*event_callback_t)(void *);
+
+typedef struct
+{
+    event_name_t     name;
+    event_callback_t callback;
+} event_pair_t;
 
 #endif /*EVENT_SCHEDULER_H_*/
